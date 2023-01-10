dea764a62b32b4a71483b728caddc30d0951e952
Thamires Marques <thamiresvieiramarques@gmail.com>
2023-01-10 18:00:48 -0300

    projeto js intermediário

---
+++ src/js/acordeon.js
@@ -0,0 +1,16 @@
+const textoPerguntasERespostas = document.querySelectorAll (".texto");
+
+textoPerguntasERespostas.forEach( function (item){
+    item.addEventListener("click", function (){
+       const itemAtivoAtual = document.querySelector(".ativo");
+       
+       if (itemAtivoAtual){
+       itemAtivoAtual.classList.remove("ativo");
+       }
+       item.classList.add("ativo");
+         
+        
+    });
+    
+});
+
