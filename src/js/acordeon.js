b35ae3c0c6ffabb15e6872103bd185f3501e5704
Thamires Marques <thamiresvieiramarques@gmail.com>
2023-01-10 18:06:04 -0300

    refatorando o código

--- src/js/acordeon.js
+++ src/js/acordeon.js
@@ -1,16 +1,12 @@
-const textoPerguntasERespostas = document.querySelectorAll (".texto");
+const textoPerguntasERespostas = document.querySelectorAll(".texto");
 
-textoPerguntasERespostas.forEach( function (item){
-    item.addEventListener("click", function (){
-       const itemAtivoAtual = document.querySelector(".ativo");
-       
-       if (itemAtivoAtual){
-       itemAtivoAtual.classList.remove("ativo");
-       }
-       item.classList.add("ativo");
-         
-        
-    });
-    
+textoPerguntasERespostas.forEach(function (item) {
+  item.addEventListener("click", function () {
+    const itemAtivoAtual = document.querySelector(".ativo");
+
+    if (itemAtivoAtual) {
+      itemAtivoAtual.classList.remove("ativo");
+    }
+    item.classList.add("ativo");
+  });
 });
-
